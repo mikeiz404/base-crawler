9e6058f85404ccbedbe4b9b5b4bde532d9e884d3
Mike Wilson <computergeekxp@gmail.com>
2017-07-28 15:48:09 -0700

    Remove unused classes

--- src/main/java/com/aggregatedbits/crawlers/base/LocatorHandler.java
+++
@@ -1,6 +0,0 @@
-package com.aggregatedbits.crawlers.base;
-
-public interface LocatorHandler<C>
-{
-	public void handle( C context, ContentLocator locator );
-}
--- src/main/java/com/aggregatedbits/crawlers/base/WorkerFinishedCallback.java
+++
@@ -1,6 +0,0 @@
-package com.aggregatedbits.crawlers.base;
-
-public interface WorkerFinishedCallback<C>
-{
-	public void onFinished( C context );
-}
